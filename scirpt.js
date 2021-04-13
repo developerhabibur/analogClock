7fa0177e448e07dfe47d7fb2cb2df25d573f7542
Md. Habibur Rahman <72272247+developerhabibur@users.noreply.github.com>
2021-04-13 16:29:53 +0600

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,23 @@
+let hr = document.getElementById('hr');
+let mn = document.getElementById('mn')
+let sc = document.getElementById('sc')
+let body = document.querySelector('body')
+let toggleClass = document.querySelector('.toggleClass')
+
+toggleClass.addEventListener('click', function(){
+    body.classList.toggle('light')
+    console.log('hii');
+})
+
+setInterval(() => {
+    let date = new Date()
+
+    let hour = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+
+    sc.style.transform = `rotate(${sec*6}deg)`
+    mn.style.transform = `rotate(${min*6}deg)`
+    hr.style.transform = `rotate(${hour*30}deg)`
+    
+}, 1000);
